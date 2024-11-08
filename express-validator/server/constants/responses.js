cb7b006be71ebcc3cf4a343d379afb7afcc80793
4444Fede <federico.vega@gm2dev.com>
2024-11-08 10:19:52 -0300

    feat(responses.js): add responses.js for standardized response messages

---
+++ express-validator/server/constants/responses.js
@@ -0,0 +1,14 @@
+export const statusCodes = {
+  OK: 200,
+  RESOURCE_CREATED: 201,
+  BAD_REQUEST_ERROR: 400,
+  UNAUTHORIZED_ERROR: 401,
+  ACCESS_FORBIDDEN: 403,
+  RESOURCE_NOT_FOUND: 404,
+  DUPLICATE_RESOURCE: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export const statusMessages = {
+  INTERNAL_ERROR: "An internal error has occurred",
+};
